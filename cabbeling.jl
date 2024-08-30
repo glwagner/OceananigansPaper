781fd3b3b3427167e8ee2fe61d9b51d2425e021b
Gregory Wagner <wagner.greg@gmail.com>
2024-08-30 15:31:53 -0400

    Update cabbeling.jl

--- cabbeling.jl
+++ cabbeling.jl
@@ -1,3 +1,4 @@
+#=
 using Oceananigans
 using Oceananigans.Models: seawater_density
 using SeawaterPolynomials: TEOS10EquationOfState
@@ -22,7 +23,7 @@ set!(model, T=Tᵢ, S=0, u=Ξᵢ, v=Ξᵢ, w=Ξᵢ)
 
 Δx = minimum_xspacing(grid)
 Δt = 0.2 * Δx^2 / closure.ν
-simulation = Simulation(model; Δt, stop_time=200)
+simulation = Simulation(model; Δt, stop_time=1000)
 
 u, v, w = model.velocities
 progress(sim) = @info string("Iter: ", iteration(sim),
@@ -36,41 +37,44 @@ T = model.tracers.T
 
 output_writer = JLD2OutputWriter(model, (; ρ, T),
                                  filename = "cabbeling",
-                                 schedule = TimeInterval(1),
+                                 schedule = TimeInterval(2),
                                  overwrite_existing = true)
                                         
 simulation.output_writers[:jld2] = output_writer
 
 run!(simulation)
+=#
+
+using CairoMakie
 
 ρt = FieldTimeSeries("cabbeling.jld2", "ρ")
 Tt = FieldTimeSeries("cabbeling.jld2", "T")
 
+Nt = length(ρt)
 n = Observable(1)
 ρ = @lift ρt[$n]
 T = @lift Tt[$n]
 
-# ρ = Field(seawater_density(model))
-# compute!(ρ)
+ρ = Field(seawater_density(model))
+compute!(ρ)
 
-using CairoMakie
 fig = Figure(size=(700, 400))
 
 axρ = Axis(fig[2, 1], aspect=1, xlabel="x (m)", ylabel="z (m)")
-hm = heatmap!(axρ, ρ, colormap=:grays) #, colorrange=(ρlo, ρhi))
+hm = heatmap!(axρ, ρ, colormap=CairoMakie.Reverse(:grays), colorrange=(999.90, 999.98))
 Colorbar(fig[1, 1], hm, label="Density (kg m⁻³)", vertical=false)
 
 axT = Axis(fig[2, 2], aspect=1, xlabel="x (m)", ylabel="z (m)")
-hm = heatmap!(axT, model.tracers.T, colormap=:thermal)
+hm = heatmap!(axT, T, colormap=:thermal, colorrange=(1.2, 7.3))
 Colorbar(fig[1, 2], hm, label="Temperature (ᵒC)", vertical=false)
 
-#save("cabbeling.png", current_figure())
-Nt = length(ρt)
-record(fig, "cabbeling.mp4", 1:Nt, framerate=24) do nn
+record(fig, "cabbeling.mp4", 1:Nt, framerate=12) do nn
+    @info "Drawing frame $nn of $Nt..."
     n[] = nn
 end
 
 #=
+#save("cabbeling.png", current_figure())
 ρmax = maximum(ρ)
 ρmin = minimum(ρ)
 ρmid = (ρmax + ρmin) / 2
