09cafb530a5b86101f1c2008dff6aaf40865011a
glwagner <gregory.leclaire.wagner@gmail.com>
2024-10-01 15:52:01 -0400

    Updates

--- listings/flow_around_cylinder.jl
+++ listings/flow_around_cylinder.jl
@@ -1,56 +1,10 @@
 using Oceananigans
-using Oceananigans.Models.NonhydrostaticModels: ConjugateGradientPoissonSolver, DiagonallyDominantPreconditioner
+using Oceananigans.Models.NonhydrostaticModels: ConjugateGradientPoissonSolver
+using Oceananigans.Models.NonhydrostaticModels: DiagonallyDominantPreconditioner
 using Oceananigans.Operators: ℑxyᶠᶜᵃ, ℑxyᶜᶠᵃ
 using Oceananigans.Solvers: FFTBasedPoissonSolver
 using Printf
 using CUDA
-
-config = :les
-u∞ = 1
-r = 1/2
-arch = CPU()
-stop_time = 100
-
-if config == :dns
-    if Re <= 10
-        Ny = 256 
-        Nx = 2Ny
-    elseif Re <= 100
-        Ny = 512 
-        Nx = 2Ny
-    elseif Re <= 1000
-        Ny = 2^11
-        Nx = 2Ny
-    elseif Re == 10^4
-        Ny = 2^12
-        Nx = 2Ny
-    elseif Re == 10^5
-        Ny = 2^13
-        Nx = 2Ny
-    elseif Re == 10^6
-        Ny = 3/2 * 2^13 |> Int
-        Nx = 2Ny
-    end
-else
-    Ny = 128
-    Nx = 2Ny
-    Re = Inf
-end
-
-prefix = "flow_around_cylinder_$(config)_Re$(Re)_Ny$(Ny)"
-
-# x = (-3, 21) # 24
-# y = (-6, 6)  # 12
-
-x = (-1.5, 10.5) # 24
-y = (-3, 3)      # 12
-
-grid = RectilinearGrid(arch, size=(Nx, Ny); x, y, halo=(6, 6), topology=(Periodic, Bounded, Flat))
-reduced_precision_grid = RectilinearGrid(arch, Float32, size=(Nx, Ny); x, y, halo=(6, 6), topology=(Periodic, Bounded, Flat))
-                       
-
-@inline cylinder(x, y) = (x^2 + y^2) ≤ r^2
-grid = ImmersedBoundaryGrid(grid, GridFittedBoundary(cylinder))
 
 @inline ϕ²(i, j, k, grid, q) = @inbounds q[i, j, k]^2
 @inline speedᶠᶜᶜ(i, j, k, grid, u, v) = @inbounds sqrt(u[i, j, k]^2 + ℑxyᶠᶜᵃ(i, j, k, grid, ϕ², v))
@@ -58,158 +12,168 @@ grid = ImmersedBoundaryGrid(grid, GridFittedBoundary(cylinder))
 @inline u_drag(i, j, k, grid, clock, f, cᴰ) = @inbounds - cᴰ * f.u[i, j, k] * speedᶠᶜᶜ(i, j, k, grid, f.u, f.v)
 @inline v_drag(i, j, k, grid, clock, f, cᴰ) = @inbounds - cᴰ * f.v[i, j, k] * speedᶜᶠᶜ(i, j, k, grid, f.u, f.v)
 
-if config == :dns
-    advection = Centered(order=2)
-    closure = ScalarDiffusivity(ν=1/Re)
+config = :les
+u∞ = 1
+r = 1/2
+arch = GPU()
+stop_time = 100
 
-    no_slip = ValueBoundaryCondition(0)
-    velocity_bcs = FieldBoundaryConditions(immersed=no_slip)
-    boundary_conditions = (u=velocity_bcs, v=velocity_bcs)
-elseif config == :les
-    advection = WENO(order=5)
-    closure = nothing
-
-    x₁ = minimum_xspacing(grid) / 2
-    ϰ = 0.4
-    ℓ = 1e-4
-    @show Cᴰ = (ϰ / log(x₁ / ℓ))^2
-    #u_drag_bc = FluxBoundaryCondition(u_drag, field_dependencies=(:u, :v), parameters=Cᴰ)
-    #v_drag_bc = FluxBoundaryCondition(v_drag, field_dependencies=(:u, :v), parameters=Cᴰ)
-    u_drag_bc = FluxBoundaryCondition(u_drag, discrete_form=true, parameters=Cᴰ)
-    v_drag_bc = FluxBoundaryCondition(v_drag, discrete_form=true, parameters=Cᴰ)
-    u_bcs = FieldBoundaryConditions(immersed=u_drag_bc)
-    v_bcs = FieldBoundaryConditions(immersed=v_drag_bc)
-    boundary_conditions = (u=u_bcs, v=v_bcs)
-end
-
-rate = 1
-x = xnodes(grid, Face())
-@inline mask(x, y, δ=6, x₀=x[end]) = max(zero(x), 1 + (x - x₀) / δ)
-u_sponge = Relaxation(target=1; mask, rate)
-v_sponge = Relaxation(target=0; mask, rate)
-forcing = (u=u_sponge, v=v_sponge)
-
-preconditioner = FFTBasedPoissonSolver(reduced_precision_grid)
-pressure_solver = ConjugateGradientPoissonSolver(grid, maxiter=100; preconditioner)
-
-#pressure_solver = ConjugateGradientPoissonSolver(grid; preconditioner=DiagonallyDominantPreconditioner())
-#pressure_solver = nothing
-
-if isnothing(pressure_solver)
-    prefix *= "_fft"
-end
-
-model = NonhydrostaticModel(; grid, pressure_solver, closure,
-                            advection, forcing, boundary_conditions)
-
-@show model
-
-uᵢ(x, y) = 1 + 1e-2 * randn()
-set!(model, u=uᵢ)
-
-Δx = minimum_xspacing(grid)
-if config == :dns
-    Δt = max_Δt = 0.2 * Δx^2 * Re
-else
-    Δt = 0.2 * Δx
-    max_Δt = Inf
-end
-
-simulation = Simulation(model; Δt, stop_iteration=100) #stop_time)
-conjure_time_step_wizard!(simulation, cfl=0.7, IterationInterval(3); max_Δt)
-
-u, v, w = model.velocities
-d = ∂x(u) + ∂y(v)
-
-# Drag computation
-μ = XFaceField(grid)
-set!(μ, mask)
-ω = u_sponge.rate
-drag_force = Field(Integral(ω * μ * (u∞ - u)))
-compute!(drag_force)
-
-wall_time = Ref(time_ns())
-
-function progress(sim)
-    if pressure_solver isa ConjugateGradientPoissonSolver
-        pressure_iters = iteration(pressure_solver)
+for Re in [Inf]
+    if config == :dns
+        if Re <= 100
+            Ny = 512 
+            Nx = 2Ny
+        elseif Re <= 1000
+            Ny = 2^11
+            Nx = 2Ny
+        elseif Re == 10^4
+            Ny = 2^12
+            Nx = 2Ny
+        elseif Re == 10^5
+            Ny = 2^13
+            Nx = 2Ny
+        elseif Re == 10^6
+            Ny = 3/2 * 2^13 |> Int
+            Nx = 2Ny
+        end
     else
-        pressure_iters = 0
+        Ny = 256
+        Nx = 3Ny
+        Re = Inf
     end
 
+    cylinder(x, y) = (x^2 + y^2) ≤ r^2
+    prefix = "flow_around_cylinder_$(config)_Re$(Re)_Ny$(Ny)"
+
+    ϵ = 0 # break up-down symmetry
+    x = (-6, 30) # 36
+    y = (-6 + ϵ, 6 + ϵ)  # 12
+
+    kw = (; size=(Nx, Ny), x, y, halo=(6, 6), topology=(Periodic, Bounded, Flat))
+    grid = RectilinearGrid(arch; kw...)
+    reduced_precision_grid = RectilinearGrid(arch, Float32; kw...)
+
+    grid = ImmersedBoundaryGrid(grid, GridFittedBoundary(cylinder))
+
+    if config == :dns
+        advection = Centered(order=2)
+        closure = ScalarDiffusivity(ν=1/Re)
+
+        no_slip = ValueBoundaryCondition(0)
+        velocity_bcs = FieldBoundaryConditions(immersed=no_slip)
+        boundary_conditions = (u=velocity_bcs, v=velocity_bcs)
+    elseif config == :les
+        advection = WENO(order=9)
+        closure = nothing
+
+        x₁ = minimum_xspacing(grid) / 2 
+        ϰ = 0.4
+        ℓ = 1e-4
+        @show cᴰ = (ϰ / log(x₁ / ℓ))^2
+        u_drag_bc = FluxBoundaryCondition(u_drag, discrete_form=true, parameters=cᴰ)
+        v_drag_bc = FluxBoundaryCondition(v_drag, discrete_form=true, parameters=cᴰ)
+        u_bcs = FieldBoundaryConditions(immersed=u_drag_bc)
+        v_bcs = FieldBoundaryConditions(immersed=v_drag_bc)
+        boundary_conditions = (u=u_bcs, v=v_bcs)
+    end
+
+    rate = 4
+    x = xnodes(grid, Face())
+    @inline mask(x, y, δ=3, x₀=27) = max(zero(x), (x - x₀) / δ)
+    u_sponge = Relaxation(target=1; mask, rate)
+    v_sponge = Relaxation(target=0; mask, rate)
+    forcing = (u=u_sponge, v=v_sponge)
+
+    ddp = DiagonallyDominantPreconditioner()
+    preconditioner = FFTBasedPoissonSolver(reduced_precision_grid)
+    reltol = abstol = 1e-7
+    pressure_solver = ConjugateGradientPoissonSolver(grid, maxiter=100;
+                                                     reltol, abstol, preconditioner)
+
+    #pressure_solver = ConjugateGradientPoissonSolver(grid, maxiter=100)
+    #pressure_solver = ConjugateGradientPoissonSolver(grid; preconditioner=ddp)
+    #pressure_solver = nothing
+
+    if isnothing(pressure_solver)
+        prefix *= "_fft"
+    end
+
+    model = NonhydrostaticModel(; grid, pressure_solver, closure,
+                                advection, forcing, boundary_conditions)
+
+    @show model
+
+    uᵢ(x, y) = 1e-1 * randn()
+    vᵢ(x, y) = 1e-1 * randn()
+    set!(model, u=uᵢ, v=vᵢ)
+
+    Δx = minimum_xspacing(grid)
+    if config == :dns
+        Δt = max_Δt = 0.2 * Δx^2 * Re
+    else
+        Δt = 0.2 * Δx
+        max_Δt = Inf
+    end
+
+    simulation = Simulation(model; Δt, stop_time)
+    conjure_time_step_wizard!(simulation, cfl=1.0, IterationInterval(3); max_Δt)
+
+    u, v, w = model.velocities
+    d = ∂x(u) + ∂y(v)
+
+    # Drag computation
+    μ = XFaceField(grid)
+    set!(μ, mask)
+    ω = u_sponge.rate
+    drag_force = Field(Integral(ω * μ * (u∞ - u)))
     compute!(drag_force)
-    D = CUDA.@allowscalar first(drag_force)
-    cᴰ = D / (u∞ * r) 
-    vmax = maximum(model.velocities.v)
-    dmax = maximum(abs, d)
 
-    msg = @sprintf("Iter: %d, time: %.2f, Δt: %.4f, Poisson iters: %d",
-                   iteration(sim), time(sim), sim.Δt, pressure_iters)
+    wall_time = Ref(time_ns())
 
-    elapsed = 1e-9 * (time_ns() - wall_time[])
+    function progress(sim)
+        if pressure_solver isa ConjugateGradientPoissonSolver
+            pressure_iters = iteration(pressure_solver)
+        else
+            pressure_iters = 0
+        end
 
-    msg *= @sprintf(", max d: %.2e, max v: %.2e, cᴰ = %0.2f, wall time: %s",
-                    dmax, vmax, cᴰ, prettytime(elapsed))
+        compute!(drag_force)
+        D = CUDA.@allowscalar first(drag_force)
+        cᴰ = D / (u∞ * r) 
+        vmax = maximum(model.velocities.v)
+        dmax = maximum(abs, d)
 
-    @info msg
+        msg = @sprintf("Iter: %d, time: %.2f, Δt: %.4f, Poisson iters: %d",
+                       iteration(sim), time(sim), sim.Δt, pressure_iters)
 
-    wall_time[] = time_ns()
+        elapsed = 1e-9 * (time_ns() - wall_time[])
 
-    return nothing
+        msg *= @sprintf(", max d: %.2e, max v: %.2e, D: %0.2f, wall time: %s",
+                        dmax, vmax, cᴰ, prettytime(elapsed))
+
+        @info msg
+        wall_time[] = time_ns()
+
+        return nothing
+    end
+
+    add_callback!(simulation, progress, IterationInterval(100))
+
+    ζ = ∂x(v) - ∂y(u)
+    outputs = (; u, v, ζ, d)
+
+    simulation.output_writers[:jld2] = JLD2OutputWriter(model, outputs,
+                                                        schedule = TimeInterval(2),
+                                                        filename = prefix * "_fields.jld2",
+                                                        overwrite_existing = true,
+                                                        with_halos = true)
+
+    simulation.output_writers[:drag] = JLD2OutputWriter(model, (; drag_force),
+                                                        schedule = TimeInterval(0.1),
+                                                        filename = prefix * "_drag.jld2",
+                                                        overwrite_existing = true,
+                                                        with_halos = true)
+     
+    run!(simulation)
 end
-
-add_callback!(simulation, progress, IterationInterval(10))
-
-ζ = ∂x(v) - ∂y(u)
-outputs = (; u, v, ζ, d)
-
-simulation.output_writers[:jld2] = JLD2OutputWriter(model, outputs,
-                                                    schedule = TimeInterval(10),
-                                                    filename = prefix * "_fields.jld2",
-                                                    overwrite_existing = true,
-                                                    with_halos = true)
-
-simulation.output_writers[:drag] = JLD2OutputWriter(model, (; drag_force),
-                                                    schedule = TimeInterval(0.1),
-                                                    filename = prefix * "_drag.jld2",
-                                                    overwrite_existing = true,
-                                                    with_halos = true)
- 
-run!(simulation)
-
-using GLMakie
-
-p = model.pressures.pNHS
-x, y, z = nodes(p)
-d = compute!(Field(d))
-ζ = compute!(Field(ζ))
-
-@show dlim = maximum(abs, d) / 2
-
-fig = Figure(size=(1500, 500))
-axz = Axis(fig[1, 1], aspect=1)
-axd = Axis(fig[1, 2], aspect=1)
-axp = Axis(fig[1, 3], aspect=1)
-heatmap!(axz, ζ)
-heatmap!(axd, d, colormap=:balance, colorrange=(-dlim, dlim))
-heatmap!(axp, x, y, interior(p, :, :, 1), colormap=:balance, colorrange=(-1, 1))
-
-xlims!(axz, -1, 3)
-ylims!(axz, -2, 2)
-xlims!(axd, -1, 3)
-ylims!(axd, -2, 2)
-xlims!(axp, -1, 3)
-ylims!(axp, -2, 2)
-display(fig)
-
-#=
-using GLMakie
-ζ = Field(ζ)
-compute!(ζ)
-
-fig = Figure(size=(1600, 500))
-ax = Axis(fig[1, 1], aspect=3)
-heatmap!(ax, ζ, colormap=:balance, nan_color=:gray)
-display(fig)
-=#
 
--- listings/prototype_omip_simulation.jl
+++ listings/prototype_omip_simulation.jl
@@ -1,42 +1,172 @@
 using Oceananigans
+using Oceananigans.Units
+
 using ClimaOcean
+using ClimaOcean.ECCO: ECCO_restoring_forcing, ECCO4Monthly, ECCO2Daily, ECCOMetadata
+
 using OrthogonalSphericalShellGrids
+using Printf
+
 using CFTime
 using Dates
 
-Nx = 2160
-Ny = 1080
-Nz = 60
-z = exponential_z_faces(Nz=60, depth=6000)
+resolution = 1/6 # degree
+Nx = round(Int, 360 / resolution)
+Ny = round(Int, 170 / resolution)
+Nz = 10
+Δt = 2minutes
+stop_time = 10days
+z_faces = exponential_z_faces(; Nz, depth=6000)
+arch = GPU() #Distributed(GPU(), partition = Partition(2))
 
-grid = TripolarGrid(GPU; size = (Nx, Ny, Nz), halo = (7, 7, 7), z)
+grid = TripolarGrid(arch; 
+                    size = (Nx, Ny, Nz), 
+                    halo = (7, 7, 7), 
+                    z = z_faces, 
+                    north_poles_latitude = 55,
+                    first_pole_longitude = 75)
 
-bottom_height = retrieve_bathymetry(grid, minimum_depth = 5, major_basins = 1,
-                                    interpolation_passes = 10)
+bottom_height = retrieve_bathymetry(grid;
+                                    minimum_depth = 10,
+                                    interpolation_passes = 2,
+                                    major_basins = 1)
  
-grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height))
+grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bottom_height); active_cells_map=true) 
+free_surface = SplitExplicitFreeSurface(grid; cfl=0.7, fixed_Δt=Δt)
 
-start_date = DateTimeProlepticGregorian(1993, 1, 1)
-end_date = DateTimeProlepticGregorian(2003, 12, 1)
-dates = range(start_date, stop=end_date, step=Month(1))
+#####
+##### Add restoring to ECCO fields for temperature and salinity in the artic and antarctic
+#####
 
-FT = ECCORestoring(arch, :temperature; dates, rate=1/30days)
-FS = ECCORestoring(arch, :salinity; dates, rate=1/30days)
-forcing = (; T=FT, S=FS)
+@inline function restoring_mask(λ, φ, z, t=0)
+    ϵN = (φ - 75) / 5
+    ϵN = clamp(ϵN, zero(ϵN), one(ϵN))
+    ϵS = - (φ + 75) / 5
+    ϵS = clamp(ϵS, zero(ϵS), one(ϵS))
+    return ϵN + ϵS
+end
 
-ocean = ocean_simulation(grid; free_surface, forcing) 
+restoring_mask_field = CenterField(grid)
+set!(restoring_mask_field, restoring_mask)
 
-backend    = JRA55NetCDFBackend(24) 
+@inline sponge_layer(λ, φ, z, t, c, ω) = - restoring_mask(λ, φ, z, t) * ω * c
+Fu = Forcing(sponge_layer, field_dependencies=:u, parameters=1/1days)
+Fv = Forcing(sponge_layer, field_dependencies=:v, parameters=1/1days)
+
+dates = DateTimeProlepticGregorian(1993, 1, 1) : Month(1) : DateTimeProlepticGregorian(2003, 12, 1)
+
+temperature = ECCOMetadata(:temperature, dates, ECCO4Monthly())
+salinity    = ECCOMetadata(:salinity,    dates, ECCO4Monthly())
+
+FT = ECCO_restoring_forcing(temperature; mask=restoring_mask_field, grid, architecture=arch, timescale=1days)
+FS = ECCO_restoring_forcing(salinity;    mask=restoring_mask_field, grid, architecture=arch, timescale=1days)
+forcing = (; T=FT, S=FS, u=Fu, v=Fv)
+
+# New advection scheme
+tracer_advection = WENO(order=5)
+momentum_advection = WENOVectorInvariant(vorticity_order=5)
+
+ocean = ocean_simulation(grid; forcing, tracer_advection, free_surface, momentum_advection) 
+
+backend = JRA55NetCDFBackend(24) 
 atmosphere = JRA55_prescribed_atmosphere(arch; backend)
-sea_ice = MinimumTemperatureSeaIce()
-coupled_model = OceanSeaIceModel(ocean, sea_ice; atmosphere)
+radiation = Radiation(arch, ocean_albedo=LatitudeDependentAlbedo())
+sea_ice = ClimaOcean.OceanSeaIceModels.MinimumTemperatureSeaIce()
+coupled_model = OceanSeaIceModel(ocean, sea_ice; atmosphere, radiation)
 
-# Spin up for a week
-simulation = Simulation(coupled_model; Δt=30, stop_time=7days)
-run!(simulation)
+@info "Set up coupled model:"
+@info coupled_model
 
-# Longer run with a long time step
-simulation.Δt = 5minutes
-simulation.stop_time = 360days
-run!(simulation)
+coupled_simulation = Simulation(coupled_model; Δt, stop_time)
+
+prefix = "prototype_omip_simulation"
+
+#=
+fluxes = (u = ocean.model.velocities.u.boundary_conditions.top.condition,
+          v = ocean.model.velocities.v.boundary_conditions.top.condition,
+          T = ocean.model.tracers.T.boundary_conditions.top.condition,
+          S = ocean.model.tracers.S.boundary_conditions.top.condition)
+
+ocean.output_writers[:fluxes] = JLD2OutputWriter(ocean.model, fluxes,
+                                                 schedule = TimeInterval(1days),
+                                                 with_halos = true,
+                                                 overwrite_existing = true,
+                                                 array_type = Array{Float32},
+                                                 filename = prefix * "surface_fluxes")
+=#
+
+outputs = merge(ocean.model.tracers, ocean.model.velocities)
+
+ocean.output_writers[:surface] = JLD2OutputWriter(ocean.model, outputs, 
+                                                  schedule = TimeInterval(1days),
+                                                  with_halos = true,
+                                                  overwrite_existing = true,
+                                                  array_type = Array{Float32},
+                                                  filename = prefix * "_surface",
+                                                  indices = (:, :, grid.Nz))
+
+#=
+ocean.output_writers[:snapshots] = JLD2OutputWriter(ocean.model, merge(ocean.model.tracers, ocean.model.velocities),
+                                                    schedule = TimeInterval(10days),
+                                                    with_halos = true,
+                                                    overwrite_existing = true,
+                                                    array_type = Array{Float32},
+                                                    filename = prefix * "snapshots")
+
+ocean.output_writers[:checkpoint] = Checkpointer(ocean.model, 
+                                                 schedule = TimeInterval(60days),
+                                                 overwrite_existing = true,
+                                                 prefix = prefix * "checkpoint")
+=#
+
+@info "Built an ocean simulation with the model:"
+@info ocean.model
+@info "\ninside the simulation:"
+@info ocean
+
+#####
+##### The atmosphere
+#####
+
+# Spin up
+set!(ocean.model, 
+     T = ECCOMetadata(:temperature, dates[1], ECCO2Daily()),
+     S = ECCOMetadata(:salinity,    dates[1], ECCO2Daily()))
+
+wall_time = Ref(time_ns())
+
+function progress(sim) 
+    ocean = sim.model.ocean
+    u, v, w = ocean.model.velocities  
+    T, S = ocean.model.tracers
+
+    Tmax = maximum(interior(T))
+    Tmin = minimum(interior(T))
+    umax = maximum(abs, interior(u)), maximum(abs, interior(v)), maximum(abs, interior(w))
+    step_time = 1e-9 * (time_ns() - wall_time[])
+
+    msg = @sprintf("Time: %s, step: %d", prettytime(sim), iteration(sim))
+
+    msg *= @sprintf(", max(u): (%.2e, %.2e, %.2e) m s⁻¹, extrema(T): %.2fᵒC, %.2fᵒC, wall time: %s",
+                   umax..., Tmax, Tmin, prettytime(step_time))
+
+    @info msg
+    wall_time[] = time_ns()
+end
+
+add_callback!(coupled_simulation, progress, IterationInterval(10))
+
+@info "Running the coupled simulation:"
+@info coupled_simulation
+    
+run!(coupled_simulation)
+
+# Run for real
+#coupled_simulation.Δt = 5minutes
+#
+## Let's reset the maximum number of iterations
+#coupled_simulation.stop_time = 7200days
+#coupled_simulation.stop_iteration = Inf
+#
+#run!(coupled_simulation)
 
