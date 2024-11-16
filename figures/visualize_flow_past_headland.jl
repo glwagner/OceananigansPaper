2b61b55aba6743e319acd1a71d0ed6b17fb061a3
Gregory Wagner <wagner.greg@gmail.com>
2024-11-16 12:15:56 -0700

    Update visualization script

--- figures/visualize_flow_past_headland.jl
+++ figures/visualize_flow_past_headland.jl
@@ -11,8 +11,8 @@ axζ = Axis(fig[2, 1], aspect=2)
 slider = Slider(fig[3, 1], range=1:Nt, startvalue=1)
 n = slider.value
 
-un = @lift ut[$n]
-ζn = @lift ζt[$n]
+un = @lift interior(ut[$n], :, :, 1)
+ζn = @lift interior(ζt[$n], :, :, 1)
 
 heatmap!(axu, un)
 heatmap!(axζ, ζn)
