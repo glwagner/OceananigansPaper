96427545f4a44251c8983c8524585242a481b6d6
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-11-18 07:03:33 +1100

    units and axes labels

--- figures/visualize_internal_tide.jl
+++ figures/visualize_internal_tide.jl
@@ -8,15 +8,17 @@ Nt = length(wt)
 T₂ = 12.421hours # period of M₂ tide constituent
 _, idx = findmin(abs.(wt.times .- 8T₂))
 
+x, _, z = nodes(wt)
+
 n = Observable(idx)
 wn = @lift wt[$n]
 
 wlim = maximum(abs, wt) * 3/4
 
 fig = Figure(size=(1300, 200))
-ax = Axis(fig[1, 1])
-heatmap!(ax, wn, colormap=:balance, colorrange=(-wlim, wlim), nan_color=:gray)
-hidedecorations!(ax)
+ax = Axis(fig[1, 1], xlabel="x (km)", ylabel="z (m)")
+hm = heatmap!(ax, x ./ 1e3, z,  wn, colormap=:balance, colorrange=(-wlim, wlim), nan_color=:gray)
+Colorbar(fig[1, 2], hm, label="vertical velocity (m s⁻¹)")
 display(fig)
 
 save("random_internal_tide.png", fig)
