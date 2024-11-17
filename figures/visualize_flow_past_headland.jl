2d02ab20b192fef86a2cea480b9f3bcc1b975bf3
Gregory Wagner <wagner.greg@gmail.com>
2024-11-17 13:08:23 -0700

    Update viz

--- figures/visualize_flow_past_headland.jl
+++ figures/visualize_flow_past_headland.jl
@@ -1,21 +1,74 @@
 using Oceananigans
+using Oceananigans.Units
+using Oceananigans.ImmersedBoundaries: mask_immersed_field!
 using GLMakie
+using Printf
+#using MathTeXEngine
 
-ζt = FieldTimeSeries("flow_past_headland.jld2", "ζ")
-ut = FieldTimeSeries("flow_past_headland.jld2", "u")
+ut = FieldTimeSeries("flow_past_headland_64_xy.jld2", "u")
+Tt = FieldTimeSeries("flow_past_headland_64_xy.jld2", "T")
 Nt = length(ut)
+x, y, z = nodes(ut)
 
-fig = Figure(size=(1200, 600))
-axu = Axis(fig[1, 1], aspect=2)
-axζ = Axis(fig[2, 1], aspect=2)
-slider = Slider(fig[3, 1], range=1:Nt, startvalue=1)
-n = slider.value
+np = (30, 290, 396)
 
-un = @lift interior(ut[$n], :, :, 1)
-ζn = @lift interior(ζt[$n], :, :, 1)
+# mask_immersed_field! is giving me a segfault.
+# This is a workaround.
+Nx, Ny, Nz, Nt = size(ut)
+for n in np
+    @inbounds for i = 1:Nx, j=1:Ny
+        if ut[i, j, 64, n] == 0
+            ut[i, j, 64, n] = NaN
+        end
 
-heatmap!(axu, un)
-heatmap!(axζ, ζn)
+        if Tt[i, j, 64, n] == 0
+            Tt[i, j, 64, n] = NaN
+        end
+    end
+
+    # We shouldn't have to do this manually either
+    # mask_immersed_field!(ut[n], NaN)
+    # mask_immersed_field!(Tt[n], NaN)
+end
+
+fig = Figure(size=(1100, 900))
+
+for (m, n) in enumerate(np)
+    axu = Axis(fig[m, 1], aspect=2, xlabel="x (m)", ylabel="y (m)")
+    axz = Axis(fig[m, 2], aspect=2, xlabel="x (m)", ylabel="y (m)", yaxisposition=:right)
+
+    un = interior(ut[n], :, :, 1)
+    Tn = interior(Tt[n], :, :, 1)
+
+    hmu = heatmap!(axu, x, y, un, nan_color=:lightgray, colormap=:balance, colorrange=(-0.3, 0.3))
+    hmT = heatmap!(axz, x, y, Tn, nan_color=:lightgray, colormap=:magma, colorrange=(9.8, 11.4))
+
+    if m == 1
+        Colorbar(fig[0, 1], hmu, vertical=false, label="x-velocity (m s⁻¹)", width=Relative(0.9))
+        Colorbar(fig[0, 2], hmT, vertical=false, label="Temperature (ᵒC)", width=Relative(0.9))
+    end
+
+    if m < 3
+        hidexdecorations!(axu)
+        hidexdecorations!(axz)
+    end
+
+    xtxt = 0.03
+    ytxt = 0.97
+    T₂ = 12.421hours
+    t = ut.times[n] * 2π/T₂
+    label = @sprintf("2π t / T₂ = %.1f", t)
+    if m == 1
+        color = :white
+    elseif m == 2
+        color = :black
+    elseif m == 3
+        color = :black
+    end
+    text!(axu, xtxt, ytxt; text=label, space=:relative, color, align=(:left, :top), fontsize=18)
+end
 
 display(fig)
+
+save("flow_past_headland.png", fig)
 
