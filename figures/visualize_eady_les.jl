e360fe1189a4f755738cf3a3e1b7883390357859
Gregory Wagner <wagner.greg@gmail.com>
2024-11-16 20:23:05 -0700

    Add visualization scripts for Eady

--- figures/visualize_eady_les.jl
+++ figures/visualize_eady_les.jl
@@ -1,86 +1,93 @@
 using Oceananigans
 using GLMakie
 
-#=
 xyfilename = "eady_les_1024_xy.jld2"
 yzfilename = "eady_les_1024_yz.jld2"
 avfilename = "eady_les_1024_av.jld2"
 
-ζt = FieldTimeSeries(xyfilename, "ζ")
-#st = FieldTimeSeries(xyfilename, "s")
-∇b²t = FieldTimeSeries(xyfilename, "∇b²")
+# wt = FieldTimeSeries(xyfilename, "w")
+# ζt = FieldTimeSeries(xyfilename, "ζ")
+# ∇b²t = FieldTimeSeries(xyfilename, "∇b²")
 
-wt = FieldTimeSeries(yzfilename, "w")
-ut = FieldTimeSeries(yzfilename, "u")
+Nt = length(wt)
+Nz = size(∇b²t, 3)
 
-Bt = FieldTimeSeries(avfilename, "b")
-N²t = FieldTimeSeries(avfilename, "N²")
-Ut = FieldTimeSeries(avfilename, "u")
-Vt = FieldTimeSeries(avfilename, "v")
-=#
+fig = Figure(size=(1300, 900))
 
-Nt = length(ζt)
-Nz = size(ζt, 3)
+axw1 = Axis(fig[2, 1], aspect=1, xlabel="x (m)", ylabel="y (m)")
+axζ1 = Axis(fig[2, 2], aspect=1, xlabel="x (m)", ylabel="y (m)")
+axb1 = Axis(fig[2, 3], aspect=1, xlabel="x (m)", ylabel="y (m)", yaxisposition=:right)
+axw2 = Axis(fig[3, 1], aspect=1, xlabel="x (m)", ylabel="y (m)")
+axζ2 = Axis(fig[3, 2], aspect=1, xlabel="x (m)", ylabel="y (m)")
+axb2 = Axis(fig[3, 3], aspect=1, xlabel="x (m)", ylabel="y (m)", yaxisposition=:right)
 
-fig = Figure(size=(1800, 1200))
+hidexdecorations!(axw1)
+hidexdecorations!(axζ1)
+hidexdecorations!(axb1)
 
-axζ = Axis(fig[1, 1], aspect=1, xlabel="x (m)", ylabel="y (m)")
-axs = Axis(fig[1, 2], aspect=1, xlabel="x (m)", ylabel="y (m)")
-axB = Axis(fig[1, 3], xlabel="Mean buoyancy (m s⁻²)", ylabel="z (m)")
-axN = Axis(fig[1, 4], xlabel="Mean vertical buoyancy gradient (s⁻²)", ylabel="z (m)")
-axw = Axis(fig[2, 1:4], xlabel="y (m)", ylabel="z (m)")
-axu = Axis(fig[3, 1:4], xlabel="y (m)", ylabel="z (m)")
+hideydecorations!(axζ1)
+hideydecorations!(axζ2)
 
-#slider = Slider(fig[4, 1:4], range=1:Nt, startvalue=1)
-#n = slider.value
-n = Observable(1)
+# slider = Slider(fig[4, 1:2], range=1:Nt, startvalue=1)
+# n = slider.value
 
-ζn = @lift ζt[$n]
-#sn = @lift st[$n]
 ∇b² = Field{Center, Center, Nothing}(∇b²t.grid)
 ∇b = Field(sqrt(∇b²))
+
+n1 = 145
+w1 = wt[n1]
+ζ1 = ζt[n1]
+
+parent(∇b²) .= parent(∇b²t[n1])
+compute!(∇b)
+∇b1 = deepcopy(∇b)
+
+n = Observable(633)
+
+wn = @lift wt[$n]
+ζn = @lift ζt[$n]
+
 ∇bn = @lift begin
     parent(∇b²) .= parent(∇b²t[$n])
     compute!(∇b)
     ∇b
 end
-wn = @lift wt[$n]
-un = @lift ut[$n]
-Bn = @lift Bt[$n]
-N²n = @lift N²t[$n]
-Un = @lift Ut[$n]
-Vn = @lift Vt[$n]
 
-hm = heatmap!(axζ, ζn, colorrange=(-1e-3, 1e-3), colormap=:balance)
-Colorbar(fig[0, 1], hm, vertical=false, label="Vertical vorticity (s⁻¹)")
+wlims = (-2e-3, 2e-3)
+ζlims = (-1e-3, 1e-3)
+blims = (0, 5e-7)
 
-hm = heatmap!(axs, ∇bn, colorrange=(0, 5e-7), colormap=:magma)
-Colorbar(fig[0, 2], hm, vertical=false, label="Horizontal buoyancy gradient (s⁻²)")
+wmap = :delta
+ζmap = :balance
+bmap = :magma
 
-hm = heatmap!(axw, wn, colorrange=(-1e-2, 1e-2), colormap=:balance)
-Colorbar(fig[2, 5], hm, vertical=true, label="Vertical velocity (m s⁻¹)")
+hm = heatmap!(axw1, w1, colorrange=wlims, colormap=wmap)
+Colorbar(fig[1, 1], hm, vertical=false, label="Vertical velocity, w (m s⁻¹)",
+         width = Relative(0.9))
 
-hm = heatmap!(axu, un, colorrange=(-1e-1, 1e-1), colormap=:balance)
-Colorbar(fig[3, 5], hm, vertical=true, label="Zonal velocity (m s⁻¹)")
+hm = heatmap!(axζ1, ζ1, colorrange=ζlims, colormap=ζmap)
+normed = [-10, -5, 0, 5, 10]
+ticks = (1e-4 .* normed, string.(normed))
+Colorbar(fig[1, 2], hm; ticks, vertical=false, label="Rossby number, ζ/f",
+         width = Relative(0.9))
 
-lines!(axB, Bn)
-lines!(axN, N²n)
+hm = heatmap!(axb1, ∇b1, colorrange=blims, colormap=bmap)
+normed = [0, 1, 2, 3, 4, 5]
+ticks = (1e-7 .* normed, string.(normed))
+Colorbar(fig[1, 3], hm; ticks, vertical=false, width = Relative(0.9),
+         label="Horizontal buoyancy gradient, |∇ₕb| (10⁻⁷ × s⁻²)")
 
-xlims!(axB, -1.2e-4, 1.2e-4)
-xlims!(axN, 0, 5e-6)
+heatmap!(axw2, wn, colorrange=wlims, colormap=wmap)
+heatmap!(axζ2, ζn, colorrange=ζlims, colormap=ζmap)
+heatmap!(axb2, ∇bn, colorrange=blims, colormap=bmap)
 
-colsize!(fig.layout, 3, Relative(0.1))
-colsize!(fig.layout, 4, Relative(0.1))
-rowsize!(fig.layout, 2, Relative(0.2))
-rowsize!(fig.layout, 3, Relative(0.2))
-
-t = ζt.times
+t = wt.times
 title = @lift "Eady turbulence at " * prettytime(t[$n])
-Label(fig[-1, 1:5], title)
+# Label(fig[0, 1:2], title)
 
 display(fig)
 
-record(fig, "eady_les.mp4", 1:Nt, framerate=12) do nn
-    @info "Drawing frame $nn of $Nt.."
-    n[] = nn
-end
+@show prettytime(t[n1])
+@show prettytime(t[n[]])
+
+save("eady_les.png", fig)
