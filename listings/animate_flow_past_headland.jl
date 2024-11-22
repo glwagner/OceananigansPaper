b5ce4d42cfb092536e30ebaaa7332d8bf0871fb4
tomchor <tomaschor@gmail.com>
2024-11-22 14:14:11 -0700

    add cooling

--- listings/animate_flow_past_headland.jl
+++ listings/animate_flow_past_headland.jl
@@ -1,11 +1,10 @@
 # Temporary script for visualization
 using Oceananigans
 using Oceananigans.Units
-using Oceananigans.ImmersedBoundaries: mask_immersed_field!
 using CairoMakie
 using Printf
 
-Nz = 32
+Nz = 64
 ut = FieldTimeSeries("flow_past_headland_$(Nz)_xy.jld2", "u")
 Tt = FieldTimeSeries("flow_past_headland_$(Nz)_xy.jld2", "T")
 wt = FieldTimeSeries("flow_past_headland_$(Nz)_xy.jld2", "w")
@@ -13,32 +12,30 @@ wt = FieldTimeSeries("flow_past_headland_$(Nz)_xy.jld2", "w")
 Nt = length(ut)
 x, y, z = nodes(ut)
 
-# mask_immersed_field! is giving me a segfault.
-# This is a workaround.
 Nz_top = ut.grid.underlying_grid.Nz
 
-fig = Figure()
+fig = Figure(size=(1100, 600))
 n = Observable(1)
 
 un = @lift interior(ut[$n], :, :, 1)
-axu = Axis(fig[1, 1], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500, height=400)
+axu = Axis(fig[1, 1], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500)
 hmu = heatmap!(axu, x, y, un, nan_color=:lightgray, colormap=:balance, colorrange=(-0.3, 0.3))
-Colorbar(fig[0, 1], hmu, vertical=false, label="x-velocity (m s⁻¹)", width=Relative(0.9))
+Colorbar(fig[0, 1], hmu, vertical=false, label="x-velocity (m s⁻¹)", width=Relative(0.5))
 
 Tn = @lift interior(Tt[$n], :, :, 1)
-axT = Axis(fig[1, 2], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500, height=400)
+axT = Axis(fig[1, 2], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500)
 hmT = heatmap!(axT, x, y, Tn, nan_color=:lightgray, colormap=:magma, colorrange=(9.8, 11.4))
-Colorbar(fig[0, 2], hmT, vertical=false, label="Temperature (ᵒC)", width=Relative(0.9))
+Colorbar(fig[0, 2], hmT, vertical=false, label="Temperature (ᵒC)", width=Relative(0.5))
 
 wn = @lift interior(wt[$n], :, :, 1)
-axw = Axis(fig[3, 1], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500, height=400)
+axw = Axis(fig[3, 1], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500)
 hmw = heatmap!(axw, x, y, wn, nan_color=:lightgray, colormap=:balance, colorrange=(-0.06, 0.06))
-Colorbar(fig[2, 1], hmw, vertical=false, label="z-velocity (m s⁻¹)", width=Relative(0.6))
+Colorbar(fig[2, 1], hmw, vertical=false, label="z-velocity (m s⁻¹)", width=Relative(0.5))
 
 ζn = @lift interior(ζt[$n], :, :, 1)
-axζ = Axis(fig[3, 2], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500, height=400)
-hmζ = heatmap!(axζ, x, y, ζn, nan_color=:lightgray, colormap=:balance, colorrange=(-0.02, 0.02))
-Colorbar(fig[2, 2], hmζ, vertical=false, label="z-vorticity (s⁻¹)", width=Relative(0.6))
+axζ = Axis(fig[3, 2], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500)
+hmζ = heatmap!(axζ, x, y, ζn, nan_color=:lightgray, colormap=:balance, colorrange=(-0.01, 0.01))
+Colorbar(fig[2, 2], hmζ, vertical=false, label="z-vorticity (s⁻¹)", width=Relative(0.5))
 
 xtxt = 0.03
 ytxt = 0.97
@@ -46,9 +43,7 @@ T₂ = 12.421hours
 title = @lift @sprintf("2π t / T₂ = %1.2f", ut.times[$n] / T₂)
 text!(axu, xtxt, ytxt; text=title, space=:relative, color=:white, align=(:left, :top), fontsize=18)
 
-
-resize_to_layout!(fig) # Resize figure after everything is done to it, but before recording
-frames = 1:5:length(ut.times)
+frames = 1:4:length(ut.times)
 record(fig, "flow_past_headland_$Nz.mp4", frames, framerate=14) do i
     msg = string("Plotting frame ", i, " of ", frames[end])
     print(msg * " \r")
--- listings/flow_past_headland.jl
+++ listings/flow_past_headland.jl
@@ -9,26 +9,33 @@ Nz = 64
 grid = RectilinearGrid(GPU(); size=(6Nz, 2Nz, Nz), halo=(6, 6, 6),
                        x, y, z, topology=(Periodic, Bounded, Bounded))
 
-bowl(y) = 1 + (y / L)^2
-wedge(x, y) = 1 + (y + abs(x)) / δ
-bowl_wedge(x, y) = -H * min(bowl(y), wedge(x, y))
-grid = ImmersedBoundaryGrid(grid, GridFittedBottom(bowl_wedge))
+wedge(x, y) = -H *(1 + (y + abs(x)) / δ)
+grid = ImmersedBoundaryGrid(grid, GridFittedBottom(wedge))
 
 @show grid
 
 T₂ = 12.421hours
-U₂ = 0.15 # m/s
-@inline Fu(x, y, z, t, p) = 2π * p.U₂ / p.T₂ * cos(2π * t / p.T₂)
+U₂ = 0.1 # m/s
+@inline Fu(x, y, z, t, p) = 2π * p.U₂ / p.T₂ * sin(2π * t / p.T₂)
 forcing = (; u=Forcing(Fu, parameters=(; U₂, T₂)))
 
 equation_of_state = SeawaterPolynomials.TEOS10EquationOfState()
 buoyancy = SeawaterBuoyancy(; equation_of_state)
 
+Q = 250.0  # W m⁻², surface _heat_ flux
+ρₒ = 1026.0 # kg m⁻³, average density at the surface of the world ocean
+cᴾ = 3991.0 # J K⁻¹ kg⁻¹, typical heat capacity for seawater
+Jᵀ = Q / (ρₒ * cᴾ) # K m s⁻¹, surface _temperature_ flux
+surface_temperature_flux(x, y, t, p) = p.Jᵀ * (1 - exp(-t^4 / (p.time_delay^4)))
+temperature_top_bc = FluxBoundaryCondition(surface_temperature_flux, parameters = (; Jᵀ, time_delay = 1day))
+temperature_bcs = FieldBoundaryConditions(top = temperature_top_bc)
+
 model = NonhydrostaticModel(; grid, tracers = (:T, :S), buoyancy, forcing,
-                            advection = WENO(order=9), coriolis = FPlane(latitude=47.5))
+                            advection = WENO(order=9), coriolis = FPlane(latitude=47.5),
+                            boundary_conditions = (; T=temperature_bcs))
 
 Tᵢ(x, y, z) = 12 + 4z / H
-set!(model, T=Tᵢ, S=32, u=0.15)
+set!(model, T=Tᵢ, S=32)
 
 simulation = Simulation(model, Δt=10, stop_time=3days)
 conjure_time_step_wizard!(simulation, cfl=0.7)
@@ -36,18 +43,20 @@ conjure_time_step_wizard!(simulation, cfl=0.7)
 using Printf
 
 wallclock = Ref(time_ns())
+T₀ = time_ns()
 
 function progress(sim)
     u, v, w = sim.model.velocities
     ΔT = 1e-9 * (time_ns() - wallclock[])
-    msg = @sprintf("(%d) t: %s, Δt: %s, wall time: %s",
-                   iteration(sim), prettytime(sim), prettytime(sim.Δt), prettytime(ΔT))
+    ΔT₀ = 1e-9 * (time_ns() - T₀)
+    msg = @sprintf("(%d) t: %s, Δt: %s, wall time / time step: %s, total wall time : %s",
+                   iteration(sim), prettytime(sim), prettytime(sim.Δt), prettytime(ΔT), prettytime(ΔT₀))
     @info msg
     wallclock[] = time_ns()
     return nothing
 end
 
-add_callback!(simulation, progress, IterationInterval(10))
+add_callback!(simulation, progress, IterationInterval(100))
 
 prefix = "flow_past_headland_$Nz"
 u, v, w = model.velocities
