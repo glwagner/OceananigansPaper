3a104a5d8ebbcd4f9306832ac3a11d11ee90ba43
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-29 17:20:15 +0100

    changes

--- listings/advection_schemes.jl
+++ listings/advection_schemes.jl
@@ -1,19 +1,41 @@
 using Oceananigans
 
+# the initial condition
+@inline G(x, β, z) = exp(-β*(x - z)^2)
+@inline F(x, α, a) = √(max(1 - α^2*(x-a)^2, 0.0))
+
+const Z = -0.7
+const δ = 0.005
+const β = log(2)/(36*δ^2)
+const a = 0.5
+const α = 10
+
+@inline function cᵢ(x)
+    if x <= -0.6 && x >= -0.8
+        return 1/6*(G(x, β, Z-δ) + 4*G(x, β, Z) + G(x, β, Z+δ))
+    elseif x <= -0.2 && x >= -0.4
+        return 1.0
+    elseif x <= 0.2 && x >= 0.0
+        return 1.0 - abs(10 * (x - 0.1))
+    elseif x <= 0.6 && x >= 0.4
+        return 1/6*(F(x, α, a-δ) + 4*F(x, α, a) + F(x, α, a+δ))
+    else
+        return 0.0
+    end
+end
+
 """
     advect_tracer(tracer_advection, closure=nothing, Nx=128)
 
 Advect a tracer with a top-hat profile until `stop_time` using `Nx` grid points.
 """
 function advect_tracer(tracer_advection; closure=nothing, Nx=128, stop_time=1)
-    grid = RectilinearGrid(size=Nx, x=(-4, 8), halo=6, topology=(Periodic, Flat, Flat))
+    grid = RectilinearGrid(size=Nx, x=(-1, 1), halo=6, topology=(Periodic, Flat, Flat))
 
-    velocities = PrescribedVelocityFields(u=1)
-    model = HydrostaticFreeSurfaceModel(; grid, closure, velocities,
-                                        tracer_advection, tracers = :c)
+    model = NonhydrostaticModel(; grid, closure, tracer_advection, tracers = :c)
 
-    set!(model, c= x -> abs(x) > 1 ? 0 : 1)
-    simulation = Simulation(model; Δt=0.1/Nx, stop_time)
+    set!(model, u=1, c=cᵢ)
+    simulation = Simulation(model; Δt=0.5/Nx, stop_time)
     run!(simulation)
     return model.tracers.c
 end
@@ -43,11 +65,11 @@ for N = (5, 11)
     lines!(ax, c, label="t=4, WENO(order=$N)")
 end
 
-solution(x) = abs(x) > 1 ? 0 : 1 
+solution(x) = cᵢ(x)
 set!(c, solution)
 lines!(ax, c, linestyle=:dash, color=(:black, 0.2), label="Initial condition")
 
-solution(x) = abs(x-stop_time) > 1 ? 0 : 1 
+solution(x) = cᵢ(x-stop_time) > 1 ? 0 : 1 
 set!(c, solution)
 lines!(ax, c, linestyle=:dash, color=(:black, 0.6), label="Exact solution at t=4")
 
