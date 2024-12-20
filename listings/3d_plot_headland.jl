8ff843107b7ff32d707acced6001109aae4afbe9
tomchor <tomaschor@gmail.com>
2024-12-20 12:47:50 -0700

    better 3d plots

--- listings/3d_plot_headland.jl
+++ listings/3d_plot_headland.jl
@@ -4,14 +4,25 @@ using Oceananigans.Units
 using GLMakie
 using Printf
 
-Nz = 12
+Nz = 32
+ut = FieldTimeSeries("flow_past_headland_$(Nz)_xyz.jld2", "u")
+wt = FieldTimeSeries("flow_past_headland_$(Nz)_xyz.jld2", "w")
 qt = FieldTimeSeries("flow_past_headland_$(Nz)_xyz.jld2", "q")
+ζt = FieldTimeSeries("flow_past_headland_$(Nz)_xyz.jld2", "ζ")
 Nt = length(qt)
 x, y, z = nodes(qt)
 
+
 H, L, δ = 256meters, 1024meters, 512meters
 grid = qt.grid.underlying_grid
-Nz_top = grid.Nz
+
+xss = 2:grid.Nx-1
+yss = 2:grid.Ny-1
+zss = 2:grid.Nz-1
+qt = qt[xss, yss, zss, :]
+x=x[xss]
+y=y[yss]
+z=z[zss]
 
 wedge(x, y) = -H *(1 + (y + abs(x)) / δ)
 bathymetry = [ wedge(x, y) > z ? 1 : 0 for x=x, y=y, z=z ]
@@ -21,14 +32,14 @@ fig = Figure(size=(1100, 600))
 settings_axis3 = (aspect = (grid.Lx, grid.Ly, 4*grid.Lz), azimuth = 0.6π, elevation = 0.2π,
                   perspectiveness=0.8, viewmode=:fitzoom, xlabel="x [m]", ylabel="y [m]", zlabel="z [m]")
 
-ax = Axis3(fig[2, 1]; settings_axis3...)
-volume!(ax, x, y, z, bathymetry, algorithm = :absorption, absorption=50f0, colormap = [:papayawhip, RGBAf(0,0,0,0), :papayawhip], colorrange=(-1, 1)) # turn on anti-aliasing
+ax = Axis3(fig[1, 1]; settings_axis3...)
+volume!(ax, x, y, z, bathymetry, algorithm = :absorption, isorange = 50, absorption=50f0, colormap = [:papayawhip, RGBAf(0,0,0,0), :papayawhip], colorrange=(-1, 1))
 
 
 colormap = to_colormap(:balance)
-PV_lim = 1e-4
-middle_chunk = ceil(Int, 0.5 * 128)
-colormap[128-middle_chunk:128+middle_chunk] .= RGBAf(0,0,0,0)
+PV_lim = 5e-1
+middle_chunk = ceil(Int, 0.4 * 128)
+colormap[128-middle_chunk:128+middle_chunk] .= RGBAf(0, 0, 0, 0)
 
 n = Observable(1)
 qₙ = @lift qt[:,:,:,$n]
@@ -38,12 +49,12 @@ Colorbar(fig, vol, bbox=ax.scene.px_area,
          alignmode = Outside(10), halign = 0.15, valign = 0.02)
 
 
-pause
 xtxt = 0.03
 ytxt = 0.97
 T₂ = 12.421hours
-title = @lift @sprintf("2π t / T₂ = %1.2f", qt.times[$n] / T₂)
-text!(axu, xtxt, ytxt; text=title, space=:relative, color=:white, align=(:left, :top), fontsize=18)
+times = ζt.times
+title = @lift @sprintf("2π t / T₂ = %1.2f", times[$n] / T₂)
+text!(ax, xtxt, ytxt; text=title, space=:relative, color=:black, align=(:left, :top), fontsize=18)
 
-frames = 1:4:length(qt.times)
+frames = 1:4:length(times)
 save("3d_plot_headland.png", fig)
