9a496c3104924deafb09d28cdccff1b83bfe7255
tomchor <tomaschor@gmail.com>
2025-01-13 16:38:57 -0700

    two 3D plots

--- figures/visualize_flow_past_headland.jl
+++ figures/visualize_flow_past_headland.jl
@@ -4,36 +4,32 @@ using GLMakie
 using Printf
 
 Nz = 64
-ut = FieldTimeSeries("flow_past_headland_$(Nz)_xy.jld2", "u")
-Tt = FieldTimeSeries("flow_past_headland_$(Nz)_xy.jld2", "T")
+Tt = FieldTimeSeries("flow_past_headland_$(Nz)_xyz.jld2", "T")
 qt = FieldTimeSeries("flow_past_headland_$(Nz)_xyz.jld2", "q")
+
+[Oceananigans.ImmersedBoundaries.mask_immersed_field!(Tt[n], Tt.grid, Oceananigans.Fields.location(Tt), NaN) for n in 1:length(Tt.times)]
 
 Nq = 5 # Time index for 3D PV plot
 time = qt.times[Nq]
 
 Nu = findmin(abs.(ut.times.-time))[2] # Find corresponding time index for 2D plots
 
-fig = Figure(size=(1100, 800))
+fig = Figure(size=(1200, 800))
 
-xu, yu, zu = nodes(ut)
-un = interior(ut[Nu], :, :, 1)
-axu = Axis(fig[1, 1], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500)
-hmu = heatmap!(axu, xu, yu, un, nan_color=:lightgray, colormap=:balance, colorrange=(-0.3, 0.3))
-Colorbar(fig[0, 1], hmu, vertical=false, label="x-velocity (m s⁻¹)", width=Relative(0.5))
-
-xT, yT, zT = nodes(Tt)
-Tn = interior(Tt[Nu], :, :, 1)
-axT = Axis(fig[1, 2], aspect=DataAspect(), xlabel="x (m)", ylabel="y (m)", width=500)
-hmT = heatmap!(axT, xT, yT, Tn, nan_color=:lightgray, colormap=:magma, colorrange=(9.8, 11.4))
-Colorbar(fig[0, 2], hmT, vertical=false, label="Temperature (ᵒC)", width=250, height=15)
 
 H, L, δ = 256meters, 1024meters, 512meters
 Nx, Ny, Nz = size(qt.grid)
 
-# Exclude edges
+# Get nodes and fields excluding edges
 xss = 2:Nx
 yss = 2:Ny
 zss = 2:Nz
+
+xT, yT, zT = nodes(Tt)
+Tt = Tt[Nx÷2:Nx, yss, zss, :]
+xT = xT[Nx÷2:Nx]
+yT = yT[yss]
+zT = zT[zss]
 
 xq, yq, zq = nodes(qt)
 qt = qt[xss, yss, zss, :]
@@ -41,15 +37,32 @@ xq = xq[xss]
 yq = yq[yss]
 zq = zq[zss]
 
+
 # Create bathymetry map
 wedge(x, y) = -H *(1 + (y + abs(x)) / δ)
 bathymetry = [ wedge(x, y) > z ? 1 : 0 for x=xq, y=yq, z=zq ]
 
-ax = Axis3(fig[2, 1:2]; aspect = (ut.grid.Lx, ut.grid.Ly, 4*ut.grid.Lz), width = 1000, height=400, azimuth = 0.6π, elevation = 0.15π,
-                        perspectiveness=0.8, viewmode=:fitzoom,
-                        xlabel="x [m]", ylabel="y [m]", zlabel="z [m]")
+axis_kwargs = (aspect = (ut.grid.Lx, ut.grid.Ly, 2*ut.grid.Lz), width = 1000, height=400, azimuth = 0.6π, elevation = 0.15π,
+               perspectiveness=0.8, viewmode=:fitzoom,
+               xlabel="x [m]", ylabel="y [m]", zlabel="z [m]")
 
-# Plot bathymetry
+ax = Axis3(fig[1, 1:2]; axis_kwargs...)
+
+volume!(ax, xq, yq, zq, bathymetry, algorithm = :absorption, isorange = 50, absorption=50f0, colormap = [RGBAf(0,0,0,0), :papayawhip], colorrange=(0, 1))
+
+Tₙ = Tt[:, :, :, Nq]
+plt = volumeslices!(ax, xT, yT, zT, Tₙ, colormap = Reverse(:roma), colorrange = (8, 12), bbox_visible = false)
+Colorbar(fig, plt, bbox=ax.scene.px_area,
+         label="Temperature (°C)", height=15, width=250, vertical=false,
+         alignmode = Outside(10), halign = -0.1, valign = 0.02)
+
+plt[:update_xy][](Nz-1)
+plt[:update_xz][](Ny-1)
+plt[:update_yz][](1)
+
+
+ax = Axis3(fig[2, 1:2]; axis_kwargs...)
+
 volume!(ax, xq, yq, zq, bathymetry, algorithm = :absorption, isorange = 50, absorption=50f0, colormap = [RGBAf(0,0,0,0), :papayawhip], colorrange=(0, 1))
 
 # adjust colormap
